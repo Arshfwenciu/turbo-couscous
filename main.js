2061d33b72e55def17c6ef11fa5f4ed6bd017ea3
Arshfwenciu <79642752+Arshfwenciu@users.noreply.github.com>
2021-04-09 19:13:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+document.elementById("result_object_name").innerHTML = result[0]. label;
+document.elementById("result_object_accuracacy").innerHTML = result[0].confidence.toFixed(3);
+{
+}function setup() {
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/[...]',modelLoaded)
+}
+
+function draw() {
+image(video, 0, 0, 300, 300);
+classfier.classify(classify, gotresult);
+}
+
+function modelLoaded() {
+console.log('Model Loaded')
+}
+
+function gotResult(error, results) {
+if(error) {
+console.error(error);
+} else {
+console.log(results);
+}
+}
